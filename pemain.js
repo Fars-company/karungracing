2b876c840d620a46ae3a32e738397fbb66aa60bf
Sans2k <140954881+Sans2k@users.noreply.github.com>
2023-09-08 16:21:13 +0700

    Add files via upload

--- pemain.js
+++ pemain.js
@@ -2,9 +2,9 @@ class Pemain {
   constructor(y,warna) {
     this.d = 40 
     this.warna = color(warna)
-    this.x = 50 
+    this.x = 20 
     this.y = y
-    this.speed = 10
+    this.speed = 20
   }
   tampilkan() {
     fill(this.warna)
@@ -15,7 +15,8 @@ class Pemain {
   }
   
   finish() {
-    if (this.x > width) {
+    if (this.x > width-20) {
+      fh.play()
       fill(this.warna)
       textSize(30)
       textAlign(CENTER)
--- sketch.js
+++ sketch.js
@@ -1,34 +1,42 @@
-let pemain1, pemain2, pemain3
+let pemain1, pemain2
+let loncat 
+let bg
+let fh
+
+function preload() {
+  soundFormats('mp3')
+  loncat = loadSound ('loncat.mp3')
+  bg = loadImage ('gambar.jpeg')
+  fh = loadSound ('finish.mp3')
+  
+}
 
 function setup() {
   createCanvas(600, 400);
   
-  pemain1 = new Pemain (100, '#65B5F5')
+  pemain1 = new Pemain (200, '#65B5F5')
   pemain2 = new Pemain (height-100, '#A4D36F')
-  pemain3 = new Pemain (height/2, 'pink')
 }
  
 function draw() {
-  background(220);
+  background(bg);
   
   pemain1.tampilkan()
   pemain2.tampilkan()
-  pemain3.tampilkan()
   
   pemain1.finish()
   pemain2.finish()
-  pemain3.finish()
+
 }
 
 
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
     pemain1.maju()
+    loncat.play()
   }
   else if (keyCode === RIGHT_ARROW) {
     pemain2.maju()
-  }
-  else if (keyCode === UP_ARROW) {
-    pemain3.maju()
+    loncat.play()
   }
 }
